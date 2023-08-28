664c1d39028cad7dd3f9ec34d687934f0913fbf3
Philippa Davies <philippadavies603@gmail.com>
2023-08-28 21:11:34 +1000

    index.html added

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+import "tw-elements";
+
+import {
+    Datepicker,
+    Input,
+    initTE,
+} from "tw-elements";
+
+initTE({ Datepicker, Input });
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
