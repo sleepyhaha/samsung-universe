1870a52dc16a1847773e4b5577c1be03eaf7a443
x.ande <ananfro@live.com>
2023-09-02 18:11:36 +1000

    cleaned server.js regarding smtp

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection.js"); //SQL connection
 const path = require("path");
 require("./models"); // Import models
-// require("./config/smtp.js");
-const {
-    createUser,
-} = require("./util/dbFunc.js");
 
 // SETUP express
 const server = express();
@@ -28,9 +24,3 @@ sequelize.sync({ force: false }).then(() => {
     server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 
 });
-
-// createUser({
-//     email: "ananfro@live.com",
-//     username: "broski69",
-//     password: "alexiscool123",
-// });
